7cc4bc6624398c4152d41bae38828671f02e621c
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-02-15 18:32:20 +0545

    light/dark function component

--- src/index.js
+++ src/index.js
@@ -2,27 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-class Theme extends React.Component {
-  state = {
-    theme: "light",
-  };
+function Theme() {
+  const [theme, setTheme] = React.useState("light");
+ 
 
-  toDark = () => this.setState({ theme: "dark" });
-  toLight = () => this.setState({ theme: "light" });
-
-  render() {
-    const { theme } = this.state;
+  const toDark = () => setTheme("dark");
+  const toLight = () => setTheme("light");
 
     return (
       <div className={theme}>
         {theme === "light" ? (
-          <button onClick={this.toDark}>Light</button>
+          <button onClick={toDark}>Light</button>
         ) : (
-          <button onClick={this.toLight}>Dark</button>
+          <button onClick={toLight}>Dark</button>
         )}
       </div>
-    )
-  };
+    );
 }
 
 ReactDOM.render(<Theme />, document.getElementById("root"));
