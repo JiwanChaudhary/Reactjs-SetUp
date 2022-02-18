c77bc4f33a6de1519df7e044091b6d5bac73ec10
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-02-18 18:52:26 +0545

    Use of module.css

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,7 @@
 import React from "react";
+import style from "./Test.module.css";
+
+console.log(style);
 
 const Test = () => {
   return <div className="dark">Test</div>;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import Test from "./Test";
+import style from "./index.module.css";
+
+console.log(style);
 
 function Theme() {
-  const [theme, setTheme] = React.useState("light");
+  const [theme, setTheme] = React.useState(style.light);
  
 
-  const toDark = () => setTheme("dark");
-  const toLight = () => setTheme("light");
+  const toDark = () => setTheme(style.dark);
+  const toLight = () => setTheme(style.light);
 
     return (
       <div className={theme}>
-        {theme === "light" ? (
+        {theme === style.light ? (
           <button onClick={toDark}>Light</button>
         ) : (
           <button onClick={toLight}>Dark</button>
