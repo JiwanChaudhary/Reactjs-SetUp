9a0bf11ce7f26ed94654c216be25022a09236503
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-02-22 15:44:04 +0545

    Pokemon App initial setUp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 
 const App = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target[3].value);
+    console.log("I am submitted");
+  }
+
   return (
     <div className="pokemon-info-app">
-      <form className="pokemon-form">
-        <label>Pokemon Name</label>
+      <form className="pokemon-form" onSubmit={handleSubmit}>
+        <label htmlFor="pokemonName-input">Pokemon Name</label>
         <small>
           Try
-          <button>"pikachu"</button>,<button>"charizard"</button>, or
-          <button>"mew"</button>
+          <button type="button">"pikachu"</button>,
+          <button type="button">"charizard"</button>, or
+          <button type="button">"mew"</button>
         </small>
 
         <div className="input-submit">
-          <input />
-          <button>Submit</button>
+          <input id="pokemonName-input" placeholder="Pokemon Name..." />
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
